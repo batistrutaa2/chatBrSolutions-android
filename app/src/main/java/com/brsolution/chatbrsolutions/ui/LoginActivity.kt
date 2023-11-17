9fbd6d5c3092a0644962e6e3830f3d8a9c8ddfd1
Celso Romao <celso.junior12@outlook.com.br>
2023-11-17 11:22:15 -0300

    criando login e validando usuario autenticado

--- app/src/main/java/com/brsolution/chatbrsolutions/ui/LoginActivity.kt
+++ app/src/main/java/com/brsolution/chatbrsolutions/ui/LoginActivity.kt
@@ -5,8 +5,22 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.brsolution.chatbrsolutions.R
 import com.brsolution.chatbrsolutions.databinding.ActivityLoginBinding
+import com.brsolution.chatbrsolutions.ultis.showMessage
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException
+import com.google.firebase.auth.FirebaseAuthInvalidUserException
+import com.google.firebase.auth.FirebaseAuthUserCollisionException
+import com.google.firebase.auth.FirebaseAuthWeakPasswordException
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var email: String
+    private lateinit var password: String
+
+
+    private val fireBaseAuth by lazy {
+        FirebaseAuth.getInstance()
+    }
 
     private val binding by lazy {
         ActivityLoginBinding.inflate(layoutInflater)
@@ -15,9 +29,22 @@ class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+        initEventsClicks()
+        //fireBaseAuth.signOut()
+    }
 
-        binding.textCadastro.setOnClickListener {
-            initEventsClicks()
+    override fun onStart() {
+        super.onStart()
+        checkUserLogged()
+    }
+
+    private fun checkUserLogged() {
+        val userCurrent = fireBaseAuth.currentUser
+
+        if (userCurrent != null) {
+            startActivity(
+                Intent(this, MainActivity::class.java)
+            )
         }
     }
 
@@ -27,5 +54,47 @@ class LoginActivity : AppCompatActivity() {
                 Intent(this, CadastroActivity::class.java)
             )
         }
+
+        binding.btnLogar.setOnClickListener {
+            email = binding.editEmail.text.toString()
+            password = binding.editSenha.text.toString()
+            if (validateInputs()) {
+                loginUser()
+            }
+        }
+    }
+
+    private fun loginUser() {
+        fireBaseAuth.signInWithEmailAndPassword(email, password).addOnSuccessListener {
+            showMessage(getString(R.string.logado_com_sucesso))
+            startActivity(
+                Intent(this, MainActivity::class.java)
+            )
+        }.addOnFailureListener { error ->
+            try {
+                throw error
+            } catch (errorUseInvalid: FirebaseAuthInvalidUserException) {
+                showMessage(getString(R.string.e_mail_n_o_cadastrado))
+            } catch (errorUseInvalid: FirebaseAuthInvalidCredentialsException) {
+                showMessage(getString(R.string.e_mail_ou_senha_incorretos))
+            }
+        }
+    }
+
+    private fun validateInputs(): Boolean {
+
+        if (email.trim().isEmpty()) {
+            binding.textInputLayoutLoginEmail.error = getString(R.string.digite_seu_email)
+            return false
+        }
+
+        if (password.isEmpty()) {
+            binding.textInputLayoutLoginEmail.error = null
+            binding.textInputLayoutLoginSenha.error = getString(R.string.digite_sua_senha)
+            return false
+        }
+        binding.textInputLayoutLoginEmail.error = null
+        binding.textInputLayoutLoginSenha.error = null
+        return true
     }
 }
